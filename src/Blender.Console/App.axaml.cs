853aa771f3743016822e133d3e53b74e25380aae
Tom Laird-McConnell <thermous@iciclecreek.com>
2026-01-30 10:42:42 -0800

    clean up a bit

--- src/Blender.Console/App.axaml.cs
+++ src/Blender.Console/App.axaml.cs
@@ -6,23 +6,11 @@ using Blender.Views;
 
 namespace Blender.Console;
 
-public partial class App : Application
+public partial class App : Blender.App
 {
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
     }
 
-    public override void OnFrameworkInitializationCompleted()
-    {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
-        {
-            desktopLifetime.MainWindow = new MainWindow
-            {
-                DataContext = new MainWindowViewModel()
-            };
-        }
-
-        base.OnFrameworkInitializationCompleted();
-    }
 }
---
+++ src/Blender/App.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Blender.ViewModels;
+using Blender.Views;
+
+namespace Blender
+{
+    public class App : Application
+    {
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
+            {
+                desktopLifetime.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel()
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
