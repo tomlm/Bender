1995d6dc95fe90bc7b26c767aa6fdf8d5b47a21f
Tom Laird-McConnell <thermous@iciclecreek.com>
2026-02-06 11:35:16 -0800

    more renaming

--- src/PrettyStick/install-local.cmd
+++ src/PrettyStick/install-local.cmd
@@ -1,4 +1,5 @@
 dotnet tool uninstall -g PrettyStick.Console
 dotnet tool uninstall -g PrettyStick
+dotnet build -c Release
 dotnet pack -c Release 
 dotnet tool install -g PrettyStick --source nupkg
